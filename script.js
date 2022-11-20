0b677cc1a5e8b67eee6dab54bef99133abefe3e4
V.Piraisoodan <piraisoodanv1996@gmail.com>
2022-11-20 18:53:13 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+setTimeout(()=>{
+    document.getElementById("calback").innerHTML=10;
+    setTimeout(()=>{
+        document.getElementById("calback").innerHTML=9;
+        setTimeout(()=>{
+            document.getElementById("calback").innerHTML=8;
+            setTimeout(()=>{
+                document.getElementById("calback").innerHTML=7;
+                setTimeout(()=>{
+                    document.getElementById("calback").innerHTML=6;
+                    setTimeout(()=>{
+                        document.getElementById("calback").innerHTML=5;
+                        setTimeout(()=>{
+                            document.getElementById("calback").innerHTML=4;
+                            setTimeout(()=>{
+                                document.getElementById("calback").innerHTML=3;
+                                setTimeout(()=>{
+                                    document.getElementById("calback").innerHTML=2;
+                                    setTimeout(()=>{
+                                        document.getElementById("calback").innerHTML=1;
+                                        setTimeout(()=>{
+                                            document.getElementById("calback").innerHTML="Advance Happy New year 2023";
+                                        },1000);
+                                    },1000);
+                                },1000);
+                            },1000);
+                        },1000);
+                    },1000);
+                },1000);
+            },1000);
+        },1000);
+    },1000);
+},1000);
